ed962595499e039cef9512c86e0325c0a4078b96
Erne <yitana2000@yahoo.com.ar>
2024-02-17 13:55:10 -0300

    video 9 GAS-Avanzado

--- funciones.js
+++ funciones.js
@@ -3,6 +3,14 @@ function doGet(){
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
 }
 
-function obtenerDatosHTML(nombre){
+function obtenerDatosHTML(nombre)
+{
    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
+
+function obtenerContactos(){
+let hoja = SpreadsheetApp.openById("1ESYTLWVpBs43pmbXle8Hu776uB7OSGQrgEroIbi63FA").getActiveSheet();
+let datos = hoja.getDataRange().getValues();
+//getValues() devuelve una matriz de objeto q hay q recorresrla para obtener c/u de esos elementos
+return datos;
 }
